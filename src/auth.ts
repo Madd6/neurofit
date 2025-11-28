ac3e6fb465cb1528be4dd822fe3dce76ec4e962f
Madd <129825850+Madd6@users.noreply.github.com>
2025-11-28 20:47:23 +0700

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,7 @@ import NextAuth, { DefaultSession } from "next-auth"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
 import { createClient } from "./utils/supabase/server"
-import { redirect } from "next/navigation"
+// import { redirect } from "next/navigation"
 
 // ✅ Minimal DB function - hanya sinkronisasi user ID
 async function syncUserToDb(userData: {
